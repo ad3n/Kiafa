4e7ea1a3c801eeeb3e2416b51f06ab308d01295b
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-01-08 15:08:08 +0700

    add home route

--- src/AppBundle/Front/HomeController.php
+++ src/AppBundle/Front/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace AppBundle\Front;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController
 {
+    /**
+     * @Route("/")
+     */
     public function indexAction(Request $request)
     {
         return new Response('Coming Soon');
