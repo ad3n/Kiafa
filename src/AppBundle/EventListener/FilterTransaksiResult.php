a994924355d70f4190224141099e624f38bf2671
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-01-08 10:07:26 +0700

    add query filter

---
+++ src/AppBundle/EventListener/FilterTransaksiResult.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use AppBundle\Controller\DonasiController;
+use AppBundle\Controller\PengeluaranController;
+use AppBundle\Entity\Transaksi;
+use Symfonian\Indonesia\AdminBundle\Event\FilterQueryEvent;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+
+class FilterTransaksiResult
+{
+    private $controller;
+
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        $controller = $event->getController();
+
+        if (!is_array($controller)) {
+            return;
+        }
+
+        if ($controller[0] instanceof DonasiController || $controller[0] instanceof PengeluaranController) {
+            $this->controller = $controller[0];
+        }
+    }
+
+    public function onFilterQuery(FilterQueryEvent $event)
+    {
+        $queryBuilder = $event->getQueryBuilder();
+        $alias = $event->getAlias();
+
+        if ($this->controller instanceof DonasiController) {
+            $queryBuilder->andWhere($queryBuilder->expr()->eq($alias.'.transactionType', Transaksi::DEBET));
+        }
+
+        if ($this->controller instanceof PengeluaranController) {
+            $queryBuilder->andWhere($queryBuilder->expr()->eq($alias.'.transactionType', Transaksi::CREDIT));
+        }
+    }
+}
