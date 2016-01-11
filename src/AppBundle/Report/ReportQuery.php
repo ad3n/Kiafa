ba856055e44597d9ff2b7fbb58e55c22930705c9
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-01-11 16:25:57 +0700

    update keterangan

--- src/AppBundle/Report/ReportQuery.php
+++ src/AppBundle/Report/ReportQuery.php
@@ -28,7 +28,7 @@ class ReportQuery
     public function getDetailPerBulan(\DateTime $dateTime)
     {
         $queryBuilder = $this->entityManager->getRepository('AppBundle:Transaksi')->createQueryBuilder('t');
-        $queryBuilder->select('t.transactionDate AS tanggal, t.transactionType AS tipe, d.fullName AS donatur, r.accountName AS rekening, t.amount AS transaksi, t.note AS keterangan');
+        $queryBuilder->select('t.transactionDate AS tanggal, t.transactionType AS tipe, d.fullName AS donatur, d.isHambaAllah as hamba, r.accountName AS rekening, t.amount AS transaksi, t.note AS keterangan');
         $queryBuilder->leftJoin('t.donatur', 'd');
         $queryBuilder->leftJoin('t.rekening', 'r');
         $queryBuilder->andWhere('MONTH(t.transactionDate) = :bulan');
