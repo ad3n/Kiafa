9eb3a3dad103dad93094b299a498dbbf5371b288
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-01-26 12:04:43 +0700

    update siab

--- src/AppBundle/Controller/DonasiController.php
+++ src/AppBundle/Controller/DonasiController.php
@@ -4,10 +4,10 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfonian\Indonesia\AdminBundle\Annotation\Schema\Crud;
-use Symfonian\Indonesia\AdminBundle\Annotation\Schema\Grid;
-use Symfonian\Indonesia\AdminBundle\Annotation\Schema\Page;
-use Symfonian\Indonesia\AdminBundle\Annotation\Schema\Util\DatePicker;
+use Symfonian\Indonesia\AdminBundle\Annotation\Crud;
+use Symfonian\Indonesia\AdminBundle\Annotation\Grid;
+use Symfonian\Indonesia\AdminBundle\Annotation\Page;
+use Symfonian\Indonesia\AdminBundle\Annotation\Util;
 use Symfonian\Indonesia\AdminBundle\Controller\CrudController;
 
 /**
@@ -17,7 +17,7 @@ use Symfonian\Indonesia\AdminBundle\Controller\CrudController;
  * @Page("page.donasi.title", description="page.donasi.description")
  * @Crud("AppBundle\Entity\Transaksi", form="AppBundle\Form\DonasiType", showFields={"transaction_date", "donatur", "amount", "note"})
  * @Grid({"transaction_date", "donatur", "amount"}, filter={"transaction_date"})
- * @DatePicker()
+ * @Util(datePicker=true)
  */
 class DonasiController extends CrudController
 {
