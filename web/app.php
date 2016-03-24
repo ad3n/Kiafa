bbe6ce12a9804205c8d99f6c4f9ed4db5d2fa23f
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-03-24 18:06:17 +0700

    activate apc

--- web/app.php
+++ web/app.php
@@ -14,15 +14,15 @@ include_once __DIR__.'/../var/bootstrap.php.cache';
 // in order to prevent cache key conflicts with other applications
 // also using APC.
 
-//$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-//$loader->unregister();
-//$apcLoader->register(true);
+$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
+$loader->unregister();
+$apcLoader->register(true);
 
 require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 Request::enableHttpMethodParameterOverride();
