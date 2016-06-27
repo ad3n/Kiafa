66424e9d37feb74e334cea1be9926090bdd0f551
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-06-27 07:18:14 +0700

    remove apc

--- web/app.php
+++ web/app.php
@@ -18,11 +18,11 @@ $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
 $loader->unregister();
 $apcLoader->register(true);
 
-require_once __DIR__.'/../app/AppCache.php';
+//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+//$kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 Request::enableHttpMethodParameterOverride();
